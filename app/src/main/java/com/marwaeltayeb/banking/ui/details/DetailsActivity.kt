a5d5df5843ec40c03974090f2060ddf2325f74b1
Marwa <marwa_eltayeb@yahoo.com>
2022-02-08 01:01:58 +0200

    Set Amount of transferred money

--- app/src/main/java/com/marwaeltayeb/banking/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/marwaeltayeb/banking/ui/details/DetailsActivity.kt
@@ -2,6 +2,9 @@ package com.marwaeltayeb.banking.ui.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.marwaeltayeb.banking.data.model.Client
 import com.marwaeltayeb.banking.databinding.ActivityDetailsBinding
 import com.marwaeltayeb.banking.util.Const.Companion.CLIENT
@@ -24,5 +27,30 @@ class DetailsActivity : AppCompatActivity() {
         binding.txtEmail.text = currentClient.email
         binding.txtPhoneNumber.text = currentClient.phoneNumber
         binding.txtBalance.text = currentClient.balance.toString()
+
+        binding.btnTransferMoney.setOnClickListener {
+            showDialog()
+        }
+
+    }
+
+    private fun showDialog(){
+        val alert: AlertDialog.Builder = AlertDialog.Builder(this)
+        val editAmount = EditText(this)
+        alert.setTitle("Enter Amount")
+        alert.setView(editAmount)
+
+        alert.setPositiveButton("Send") { _, _ ->
+            val amount: String = editAmount.text.toString()
+
+            if (amount.isNotEmpty()) {
+                Toast.makeText(applicationContext, amount, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        alert.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.cancel()
+        }
+        alert.show()
     }
 }
