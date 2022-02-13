059e3d6a7be5dff58b69266666b22e055c16d84b
Marwa <marwa_eltayeb@yahoo.com>
2022-02-13 16:38:36 +0200

    Add Validations

--- app/src/main/java/com/marwaeltayeb/banking/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/marwaeltayeb/banking/ui/details/DetailsActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.marwaeltayeb.banking.R
 import com.marwaeltayeb.banking.data.model.Client
 import com.marwaeltayeb.banking.databinding.ActivityDetailsBinding
 import com.marwaeltayeb.banking.ui.transfer.TransferActivity
 import com.marwaeltayeb.banking.util.Const.Companion.CLIENT
-
 import com.marwaeltayeb.banking.util.Const.Companion.AMOUNT
 import com.marwaeltayeb.banking.util.Const.Companion.TRANSFEROR
+import android.view.View
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -43,32 +42,44 @@ class DetailsActivity : AppCompatActivity() {
 
     }
 
-    private fun showDialog(){
+
+    private fun showDialog() {
         val alert: AlertDialog.Builder = AlertDialog.Builder(this)
         val editAmount = EditText(this)
-        editAmount.setInputType(InputType.TYPE_CLASS_NUMBER)
+        editAmount.inputType = InputType.TYPE_CLASS_NUMBER
         editAmount.setRawInputType(Configuration.KEYBOARD_12KEY)
         editAmount.hint = "Amount"
         editAmount.setSingleLine()
         alert.setTitle("Enter Amount")
         alert.setView(editAmount)
 
-        alert.setPositiveButton("Send") { _, _ ->
-            val amountString: String = editAmount.text.toString()
+        alert.setPositiveButton("OK") { dialog, which -> }
+        alert.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
 
-            if (amountString.isNotEmpty()) {
-                Toast.makeText(applicationContext, amountString, Toast.LENGTH_SHORT).show()
-                val amount: Double = amountString.toDouble()
-                intent = Intent(this, TransferActivity::class.java)
-                intent.putExtra(TRANSFEROR, currentClient.name)
-                intent.putExtra(AMOUNT, amount)
-                startActivity(intent)
-            }
-        }
+        val dialog = alert.create()
 
-        alert.setNegativeButton("Cancel") { dialog, _ ->
-            dialog.cancel()
-        }
-        alert.show()
+        dialog.show()
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            .setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    val amountString: String = editAmount.text.toString().trim()
+                    if (amountString.isEmpty()) {
+                        editAmount.error = "Amount can not be empty"
+                        return
+                    } else {
+                        val amount: Double = amountString.toDouble()
+                        if (amount > currentClient.balance){
+                            editAmount.error = "You do not have enough balance"
+                            return
+                        }
+                        intent = Intent(applicationContext, TransferActivity::class.java)
+                        intent.putExtra(TRANSFEROR, currentClient.name)
+                        intent.putExtra(AMOUNT, amount)
+                        startActivity(intent)
+                        dialog.dismiss()
+                    }
+                }
+            })
     }
 }
